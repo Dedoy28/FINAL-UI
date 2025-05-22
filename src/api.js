5c98f2602d5c93bc343a45e2ec6945d57ec89bc0
Dedoy28 <malana.andre28@gmail.com>
2025-05-22 20:12:16 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,6 @@
-import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
-
-export const getPosts = () => axios.get(`${API_BASE_URL}/posts`);
-export const createPost = (post) => axios.post(`${API_BASE_URL}/posts`, post);
-export const updatePost = (id, post) => axios.put(`${API_BASE_URL}/posts/${id}`, post);
-export const deletePost = (id) => axios.delete(`${API_BASE_URL}/posts/${id}`);
-
+export const getPosts = () => axios.get(`${API_URL}/posts`);
+export const createPost = (post) => axios.post(`${API_URL}/posts`, post);
+export const updatePost = (id, post) => axios.put(`${API_URL}/posts/${id}`, post);
+export const deletePost = (id) => axios.delete(`${API_URL}/posts/${id}`);
