9636e63deb603bb7f350dc8049cb4a999ae60332
Dedoy28 <malana.andre28@gmail.com>
2025-05-22 12:29:58 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
-import NavBar from './components/NavBar';
+import NavBar from './components/Navbar';
 
 
 function App() {
